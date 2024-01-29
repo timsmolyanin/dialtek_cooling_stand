30686a1eeab0c9add26594140764a639c2aefb8e
Smolyanin Timofey <tim.smolyanin@gmail.com>
2024-01-29 23:57:41 +0300

    added nextion_mqtt_module

---
+++ list_of_mqtt_topics.py
@@ -0,0 +1,4 @@
+list_of_mqtt_topics = [ ("/devices/HeaterModule/controls/Output Voltage State", 0),
+                        ("/devices/MeasureModuleConfigs/controls/CH1 ConfigFname", 0),
+                        ("/devices/MeasureModuleConfigs/controls/CH1 FilterBufferSize", 0),
+]
